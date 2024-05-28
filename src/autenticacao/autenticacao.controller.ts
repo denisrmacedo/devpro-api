250aa88b1640b7566e7618b54e89cfac4cfbf6be
Jose Wiltomar Duarte <wiltomar@prothon.ewtwech.local>
2024-05-28 18:25:16 -0300

    Correção de merging...

--- src/autenticacao/autenticacao.controller.ts
+++ src/autenticacao/autenticacao.controller.ts
@@ -1,18 +1,3 @@
-<<<<<<< HEAD
-import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
-import { AutenticacaoService } from './autenticacao.service';
-
-@Controller('autenticacao')
-export class AutenticacaoController {
-  constructor(private readonly autenticacaoService: AutenticacaoService) { }
-
-  @HttpCode(HttpStatus.OK)
-  @Post('login')
-  login(@Body() credencial: Record<string, string>): Promise<{ token: string }> {
-    return this.autenticacaoService.login(credencial.email, credencial.senha);
-  }
-}
-=======
 import { Body, Controller, Get, HttpCode, HttpStatus, Ip, Post, Request } from '@nestjs/common';
 
 import { Public } from './public.decorator';
@@ -41,4 +26,3 @@ export class AutenticacaoController {
     };
   }
 }
->>>>>>> ed13384471729ea0469223724bd2d02856d63223
